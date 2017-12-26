1962a1bd2f0432f602698c36f9a0d8d132fe6b0b
Luka Klacar <l.klacar@vegaitsourcing.rs>
2017-12-26 10:51:05 +0100

    Added rabbitmq support

--- src/main/java/com/lukaklacar/bulkemail/BulkEmailServiceApp.java
+++ src/main/java/com/lukaklacar/bulkemail/BulkEmailServiceApp.java
@@ -2,15 +2,22 @@ package com.lukaklacar.bulkemail;
 
 import com.lukaklacar.bulkemail.config.ApplicationProperties;
 import com.lukaklacar.bulkemail.config.DefaultProfileUtil;
-
 import io.github.jhipster.config.JHipsterConstants;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.*;
+import org.springframework.boot.actuate.autoconfigure.MetricFilterAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.MetricRepositoryAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
@@ -24,6 +31,40 @@ import java.util.Collection;
 @EnableAutoConfiguration(exclude = {MetricFilterAutoConfiguration.class, MetricRepositoryAutoConfiguration.class})
 @EnableConfigurationProperties({ApplicationProperties.class})
 public class BulkEmailServiceApp {
+
+
+    final static String queueName = "spring-boot";
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange("spring-boot-exchange");
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(queueName);
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
 
     private static final Logger log = LoggerFactory.getLogger(BulkEmailServiceApp.class);
 
---
+++ src/main/java/com/lukaklacar/bulkemail/Runner.java
@@ -0,0 +1,40 @@
+package com.lukaklacar.bulkemail;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final Receiver receiver;
+    private final ConfigurableApplicationContext context;
+
+    public Runner(Receiver receiver, RabbitTemplate rabbitTemplate,
+                  ConfigurableApplicationContext context) {
+        this.receiver = receiver;
+        this.rabbitTemplate = rabbitTemplate;
+        this.context = context;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Sending message...");
+
+
+        //context.close();
+    }
+
+
+    @Scheduled(fixedDelay = 1000)
+    public void start() throws InterruptedException {
+        rabbitTemplate.convertAndSend(BulkEmailServiceApp.queueName, "Hello from RabbitMQ!");
+        receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+    }
+
+}
